bf920368a2ce3e2d7d8be11a3b38904e461e483d
Igoryarets <igoryarets338@gmail.com>
2022-02-11 19:14:53 +0300

    change docker-compose

--- backend/foodgram/recipes_api/filter.py
+++ backend/foodgram/recipes_api/filter.py
@@ -17,7 +17,7 @@ class RecipeFilter(FilterSet):
 
     def filters_favorited(self, queryset, name, value):
         if value and self.request.user.is_authenticated:
-            return queryset.filter(favorites__user=self.request.user)
+            return queryset.filter(favorite_recipe__user=self.request.user)
         return queryset
 
     def filters_shopping_cart(self, queryset, name, value):
